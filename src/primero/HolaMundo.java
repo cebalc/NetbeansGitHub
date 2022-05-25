7daad68c891e554fdac7af860af0ea199fbe3716
Eric Ceballos Alcantarilla <Eric.Ceballos.Alcantarilla@gmail.com>
2022-05-25 20:20:36 +0200

    Segundo commit con Netbeans

--- src/primero/HolaMundo.java
+++ src/primero/HolaMundo.java
@@ -12,5 +12,6 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("Esta línea es nueva desde el último commit");
     }
 }
